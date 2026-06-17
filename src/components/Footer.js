dd87ebb1d9a306f52bee53bf4ac857238beecffc
Darshan M B <109517000+darshanmb7096@users.noreply.github.com>
2026-06-17 11:31:16 +0530

    Update footer link to GitHub portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer(props) {
             <footer className={`bg-body- text-center text-lg-start text-${props.mode === 'light' ? 'dark' : 'light'} bg-${props.mode} mt-auto ${location.pathname === '/' || location.pathname === '/about' ? 'footer fixed-bottom' : ''}`}>
                 <div className={`text-center p-3 text-${props.mode === 'light' ? 'dark' : 'light'}`} style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
                     © 2023 Copyright: Developed by
-                    <strong> <a className={`text-body-${props.mode === 'light' ? 'dark' : 'light'}`} style={{ textDecoration: 'none', color: props.mode === 'light' ? 'black' : 'white' }} href="https://darsion.netlify.app" target='_blank' rel="noopener noreferrer"> DARSHAN MB</a></strong>
+                    <strong> <a className={`text-body-${props.mode === 'light' ? 'dark' : 'light'}`} style={{ textDecoration: 'none', color: props.mode === 'light' ? 'black' : 'white' }} href="https://github.com/darshanmb7096/PORTFOLIO2026" target='_blank' rel="noopener noreferrer"> DARSHAN MB</a></strong>
                 </div>
             </footer>
         </div>
